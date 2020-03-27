4e8d55bfc42356f9fd1d331338f32fffda0610db
Aman Aswal <amanpotato11@gmail.com>
2020-03-27 13:30:25 +0530

    Add files via upload

---
+++ Array/1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int size_of_array;
+    cout<<"Enter the size of the array: ";
+    cin>> size_of_array;
+
+    int arr[size_of_array];
+    cout<<"Enter the numbers: "<<endl;
+
+    for(int i=0; i<size_of_array; i++)
+    {
+        cin>> arr[i];
+    }
+
+    cout<<"[ ";
+    for (int i=0; i<size_of_array;i++)
+    {   
+      
+        cout<<arr[i]<<" ";
+        
+    }
+    cout<<"]";
+    return 0;
+}
+    
