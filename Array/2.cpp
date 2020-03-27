b6d4da549fc73e792673bd6c1f3b490e3a2c91d8
Aman Aswal <amanpotato11@gmail.com>
2020-03-27 13:22:13 +0530

    Add files via upload

---
+++ Array/2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int size_of_array, num;
+    cout<<"Enter the size of the array: ";
+    cin>> size_of_array;
+
+    int arr[size_of_array];
+    cout<<"Enter the numbers: "<<endl;
+
+    for(int i=0; i<size_of_array; i++)
+    {
+        cin>> arr[i];
+    }
+
+    cout<<"Enter a number to check: ";
+    cin>>num;
+
+    for (int i=0; i<size_of_array;i++)
+    {
+        if (arr[i]==num){
+            cout<<"Present";
+            break;
+        }       
+        else if( i==(size_of_array - 1) && arr[i] != num){
+            cout<<"Not present";
+        }
+    }
+    return 0;
+}
+    
