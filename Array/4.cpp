4a7e5dc95ebe48b74bf12630dc0b5a8469606289
Aman Aswal <amanpotato11@gmail.com>
2020-03-28 13:49:57 +0530

    Update 4.cpp

--- Array/4.cpp
+++ Array/4.cpp
@@ -1,3 +1,8 @@
+QUESTION: Take integer inputs from user and store them in an array. 
+          Now, copy all the elements in another array.
+          Print the new array in reverse order.
+-----------------------------------------------------------------------    
+    
 #include<iostream>
 
 using namespace std;
@@ -17,12 +22,12 @@ int main()
         cin>>arr[i];
     }
 
-    for( int i=0; i<size_of_array; i++)         
+    for( int i=0; i<size_of_array; i++)  // copying the array
     {
         arr1[i] = arr[i];
     }
 
-    for( int i=0; i<size_of_array; i++)
+    for( int i=0; i<size_of_array; i++)  // printing the new array in reverse order
     {
         cout<<arr1[(size_of_array -1) - i]<<" "; 
     }
