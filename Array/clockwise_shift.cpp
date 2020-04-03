d20c18462ba343c91c068899323c1893066e7570
Aman Aswal <amanpotato11@gmail.com>
2020-04-03 09:45:37 +0530

    Add files via upload

---
+++ Array/clockwise_shift.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+   int size_of_array;
+   cout<<"Enter the size of array: ";
+   cin>>size_of_array;
+
+   int arr[size_of_array];
+   cout<<"Enter the elements: "<<endl;
+   
+   for(int i=0; i<size_of_array; i++)
+   {
+       cin>>arr[i];
+   }
+
+   int temp = arr[0];
+
+   for(int i=0; i<size_of_array; i++)
+   {
+       arr[i] = arr[ i+1 ];
+   }
+
+    arr[size_of_array - 1] = temp;
+
+    cout<<"\nAnti-Clockwise shift array is: ";
+
+    for ( int i=0; i<size_of_array; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
